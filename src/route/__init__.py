14d61c5da4e1c89887aba3f454a4b757766c48da
Alex Kersten <alex@kersten.email>
2017-04-17 00:01:09 -0500

    add route skeleton

---
+++ src/route/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+from . import generic
+
+
+def init_routes(flask_app: Flask):
+    flask_app.add_url_rule("/", "splash", methods=["GET"], view_func=generic.splash)
---
+++ src/route/generic.py
@@ -0,0 +1,22 @@
+# ######################################################################################################################
+# Generic top-level routes like the splash screen.
+# ######################################################################################################################
+
+from flask import render_template
+
+
+def splash():
+    """
+    The splash screen for the CutCash homepage.
+    :return: The rendered template.
+    """
+    return generic_path_render("splash.html")
+
+
+def generic_path_render(file):
+    """
+    A generic renderer for a static page.
+    :param file: The filename to render.
+    :return: The rendered template for a static page.
+    """
+    return render_template(file)

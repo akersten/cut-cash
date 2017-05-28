d712b200614284534d83c1f4d77256951d8a244e
Alex Kersten <alex@kersten.email>
2017-05-28 01:19:52 -0500

    fix redux resolution

---
+++ src/ts/redux-reducers/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by akersten on 5/27/17.
+ */
+
+import {combineReducers} from "redux";
+
+const mainReducer = combineReducers({});
+
+export default mainReducer;
---
+++ src/ts/redux-reducers/receiptReducer.ts
@@ -0,0 +1,19 @@
+import {Receipt} from "../core/receipt/receipt";
+/**
+ * Created by akersten on 5/27/17.
+ *
+ * Reducer for a receipt object.
+ */
+
+const initialState = {
+    receipt: new Receipt("Untitled", new Date()),
+};
+
+
+
+
+function receiptReducer(state = initialState, action) {
+    switch (action.type) {
+        case
+    }
+}

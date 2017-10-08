45fd35077c113e80bca20dbc6f9f242a80e63e3f
Alex Kersten <alex@kersten.email>
2017-10-08 14:31:52 -0500

    fix parsing

--- src/ts/core/lib/util/ParseHelpers.ts
+++ src/ts/core/lib/util/ParseHelpers.ts
@@ -9,6 +9,8 @@ export class ParseHelpers {
     public static parseNumber<typeOfRawValue>(userInput: string): ParseResult<typeOfRawValue> {
         // TODO: Allow commas in user input? If we do, we'll need to take them out here.
 
+        userInput = userInput + ""; // JS decides this is a number at some point, so force it to a string.
+
         if (userInput.length === 0) {
             return new ParseResult(true, null);
         }
@@ -17,6 +19,10 @@ export class ParseHelpers {
         if (!numberRegEx.test(userInput)) {
             return new ParseResult(false, null, "Not a valid number.");
         }
+
+        // Convert decimal separator to period
+        let replaceRegEx = new RegExp(RegExHelpers.escape(LocaleHelpers.getDecimalSeparator()), "g");
+        userInput = userInput.replace(replaceRegEx, ".");
 
         return new ParseResult<typeOfRawValue>(true, <typeOfRawValue><any>parseFloat(userInput));
     }
@@ -27,14 +33,23 @@ export class ParseHelpers {
             return new ParseResult(true, null);
         }
 
-        let currencyRegEx = new RegExp("^-?\\s*" + RegExHelpers.escape(LocaleHelpers.getCurrencySymbol()) + "?\\s*\\d+(" + RegExHelpers.escape(LocaleHelpers.getDecimalSeparator()) + "\\d{" + LocaleHelpers.getCurrencyDecimalPlaces() + "})?$");
+        userInput = userInput + ""; // JS decides this is a number at some point, so force it to a string.
+
+        let currencyRegEx = new RegExp("^-?\\s*" + RegExHelpers.escape(LocaleHelpers.getCurrencySymbol()) + "?\\s*\\d+(" + RegExHelpers.escape(LocaleHelpers.getDecimalSeparator()) + "\\d{" + LocaleHelpers.getCurrencyDecimalPlaces() + "})?\\s*" + RegExHelpers.escape(LocaleHelpers.getCurrencySymbol()) + "?$");
         if (!currencyRegEx.test(userInput)) {
             return new ParseResult(false, null, "Not a valid currency amount.");
         }
 
-        // TODO: Need to strip out formatting characters before trying to parse the underlying raw value.
+        // Strip out formatting characters (currency symbol and comma separator).
+        let replaceRegEx = new RegExp(RegExHelpers.escape(LocaleHelpers.getCurrencySymbol()), "g");
+        userInput = userInput.replace(replaceRegEx, "");
 
-        return new ParseResult<typeOfRawValue>(true, <typeOfRawValue><any>parseFloat(userInput));
+        // Convert decimal separator to period
+        replaceRegEx = new RegExp(RegExHelpers.escape(LocaleHelpers.getDecimalSeparator()), "g");
+        userInput = userInput.replace(replaceRegEx, ".");
+
+        // Round to # of decimal places for currency.
+        return new ParseResult<typeOfRawValue>(true, <typeOfRawValue><any>+(parseFloat(userInput)).toFixed(LocaleHelpers.getCurrencyDecimalPlaces()));
     }
 
 }
